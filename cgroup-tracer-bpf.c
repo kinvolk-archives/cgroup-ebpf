938bcb243ad794f878e3d683fd748ee2a5385e84
Iago López Galeiras <iago@kinvolk.io>
2017-01-18 17:47:09 +0100

    Counting packets example

--- cgroup-tracer-bpf.c
+++ cgroup-tracer-bpf.c
@@ -3,16 +3,17 @@
 #include "bpf_helpers.h"
 
 struct bpf_map_def SEC("maps/count") count_map = {
-	.type = BPF_MAP_TYPE_HASH,
+	.type = BPF_MAP_TYPE_ARRAY,
 	.key_size = sizeof(int),
 	.value_size = sizeof(__u64),
 	.max_entries = 1024,
 };
 
+SEC("cgroup/skb")
 int count_packets(struct pt_regs *ctx)
 {
 	int packets_key = 0;
-	u64 *packets = 0;
+	u64 *packets = NULL;
 
 	packets = bpf_map_lookup_elem(&count_map, &packets_key);
 	if (packets == NULL)
@@ -20,7 +21,8 @@ int count_packets(struct pt_regs *ctx)
 
 	*packets += 1;
 
-	return 0;
+	// don't drop
+	return 1;
 }
 
 char _license[] SEC("license") = "GPL";
